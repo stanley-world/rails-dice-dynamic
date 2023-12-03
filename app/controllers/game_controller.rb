1886bbc6e72fd3591879baa4ebf37786e8f4c4ab
stanley-world <146215090+stanley-world@users.noreply.github.com>
2023-12-03 00:50:40 +0000

    updated all

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,6 +1,30 @@
 class GameController < ApplicationController
   def user_two_six
-    
+    @roll = rand(1..6)
+    @roll_two = rand(1..6)
     render({ :template => "game_templates/two_six"})
+  end
+
+  def user_two_ten
+    @roll = rand(1..10)
+    @roll_two = rand(1..10)
+    render({ :template => "game_templates/two_ten"})
+  end
+  def user_one_twenty
+    @roll = rand(1..20)  
+    render({ :template => "game_templates/one_twenty"})
+  end
+  def user_five_four
+    @roll = rand(1..4)
+    @roll_two = rand(1..4)
+    @roll_three = rand(1..4)
+    @roll_four = rand(1..4)
+    @roll_five = rand(1..4)
+   
+    render({ :template => "game_templates/five_four"})
+  end
+  def user_home
+    
+    render({ :template => "game_templates/home"})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
 get("/dice/2/6",{:controller => "game", :action => "user_two_six"})
+get("/dice/2/10",{:controller => "game", :action => "user_two_ten"})
+get("/dice/1/20",{:controller => "game", :action => "user_one_twenty"})
+get("/dice/5/4",{:controller => "game", :action => "user_five_four"})
+get("/",{:controller => "game", :action => "user_home"})
+
+get("/dice") do 
+  @num_dice = params.fetch("dice").to_i
+
+  @num_sides = params.fetch("sides").to_i 
+
+  @rolls = []
+
+  @num_dice.times do 
+    @rolls.push(rand(1..@num_sides))
+  end
+
+  #erb(:results)
+end
 end
